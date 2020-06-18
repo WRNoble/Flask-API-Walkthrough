b4062e3f27aa77fd718b4502041f9ab9139b68e6
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-17 20:09:22 -0400

    got some classes done

--- models.py
+++ models.py
@@ -6,3 +6,20 @@ class Course(Model):
     title = CharField()
     url = CharField(unique=True)
     created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = Database
+
+class Review(Model):
+    course = ForeignKeyField(Course, related_name='review_set')
+    rating = integerField()
+    comment = TextField(default='')
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = Database
+
+def initialize():
+    Database.connect()
+    Database.create_tables([Course, Review], safe=True)
+    Database.close()
